8179e4faa336f05ffb2c6f250c66617a00aec0cd
Hendro Aditya Nurullah <aditya.nurullah@gmail.com>
2020-06-14 12:39:56 +0700

    update again

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -11,6 +11,11 @@ import {
 
 @Controller('cats')
 export class CatsController {
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats.';
+  }
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `This action returns a #${id} cat`;
